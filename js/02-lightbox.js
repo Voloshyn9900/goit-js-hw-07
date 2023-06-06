91404844a674678a3417b7ccb4f75ba9fcf53613
Yurii Voloshyn <voloshyn9900@gmail.com>
2023-06-06 21:52:09 +0200

    cimmit3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,8 +9,7 @@ galleryEl.innerHTML = createGalleryItemsMurkup(galleryItems);
 // galleryEl.addEventListener("click", onImgClick);
 
 function createGalleryItemsMurkup(galleryItems) {
-  return galleryItems
-    .map((item) => {
+  return galleryItems.map((item) => {
         return `
         <a class="gallery__item" href="${item.original}">
             <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
